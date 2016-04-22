bd1874d374a6cffb2e4e264e809c83ee13728507
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2016-04-22 09:54:10 +0200

    Initial libgdx setup

---
+++ desktop/src/tv/rocketbeans/rbcgj/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package tv.rocketbeans.rbcgj.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import tv.rocketbeans.rbcgj.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
---
+++ html/src/tv/rocketbeans/rbcgj/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package tv.rocketbeans.rbcgj.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import tv.rocketbeans.rbcgj.Game;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Game();
+        }
+}
