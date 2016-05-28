32bfaf3731f9b2300167d9a99aace5a6e3d1cc30
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2016-05-28 13:44:03 +0200

    Version bump

--- core/src/tv/rocketbeans/rbcgj/GameConfig.java
+++ core/src/tv/rocketbeans/rbcgj/GameConfig.java
@@ -3,14 +3,14 @@ package tv.rocketbeans.rbcgj;
 public interface GameConfig {
 
     String GAME_NAME = "The Legend of Studentenfutter";
-    String GAME_VERSION = "1.1.2";
+    String GAME_VERSION = "1.3.0";
 
     // Scale of cells
     int CELL_SCALE = 32;
 
     // Time to take in order to
     // move to the next cell
-    float MOVEMENT_TIME = 0.26f;
+    float MOVEMENT_TIME = 0.28f;
 
     // Auto fullscreen enabled
     boolean AUTO_FULLSCREEN = true;
